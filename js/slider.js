ba95b8d2c5d592101e37e5bacbde3b0fc84a49fd
Yulia <62777149+Sivonenko@users.noreply.github.com>
2023-09-19 13:22:19 +0300

    add

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,7 @@
-$('.variable-width').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
+
